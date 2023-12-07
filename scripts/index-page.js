a8366bb1de80a34484fc589def1f2afafcc3ed29
tank3ngine <117690704+tank3ngine@users.noreply.github.com>
2023-12-07 01:52:54 -0500

    add new comment input fields

---
+++ scripts/index-page.js
@@ -0,0 +1,10 @@
+let commentsArr = [];
+let commentObj = {
+    name: 'commenter name',
+    timestamp: 'time and date',
+    text: 'comment text'
+}
+
+function displayComment(commentObject){
+    
+}
