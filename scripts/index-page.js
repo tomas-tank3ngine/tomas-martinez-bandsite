1503c357346d9f822b3ff954eaafc7a47814f942
tank3ngine <117690704+tank3ngine@users.noreply.github.com>
2023-12-08 01:11:49 -0500

    fix styling bug for comment portraits

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,6 +5,23 @@ let commentObj = {
     text: 'comment text'
 }
 
+//<li class="comment-entry">
+//<div class="comment-entry__headshot-container">
+//    <div class="comment-entry__headshot-portrait"></div>
+//</div>
+//<!-- Comment -->
+//<div class="comment-entry__container">
+//    <div class="comment-entry__info">
+//        <h3 class="comment-entry__user-name">Connor Walton</h3>
+//        <h3 class="comment-entry__timestamp">02/17/2021</h3>
+//    </div>
+//
+//    <div class="comment-entry__comment">
+//        <p class="comment-entry__text"></p>
+//    </div>
+//</div>
+//</li>
+
 function displayComment(commentObject){
-    
+
 }
