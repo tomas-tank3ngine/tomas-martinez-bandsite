4a58d25762d51805af1d557d9c7f348f1328c929
tank3ngine <117690704+tank3ngine@users.noreply.github.com>
2023-12-17 02:51:38 -0500

    add comments API POST and sorting by timestamp

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,3 +1,4 @@
+// import axios from 'axios';
 const api_key = "abaf0f0f-c151-4d89-b72a-7e05e264f0e7";
 //?api_key=abaf0f0f-c151-4d89-b72a-7e05e264f0e7
 
@@ -11,25 +12,41 @@ class BandSiteApi{
     constructor(apiKey){
         this.apiKey = apiKey;
         this.baseUrl = "https://project-1-api.herokuapp.com/";
-        // this.testingVar = 10;
     }
     
-    async postComment(comment){
+    async postComment(commentObj){
+        //store the api url including key
+        const COMMENTS_API_URL = this.baseUrl + "comments?api_key=" + this.apiKey;
+
+        //the content type needs to be a variable like this:
+        const headers = {
+            "Content-Type": "application/json"
+        }
+
         try{
-            const response = await axios.post(this.baseUrl + "/comments");
+            await axios.post(COMMENTS_API_URL, commentObj, {headers});
 
         } catch(error){
-            console.error(error);
+            console.error("error posting comment: " + error);
         }
     }
 
     async getComments(){
-        // console.log("in comments")
+        //store the api url including key
+        const COMMENTS_API_URL = this.baseUrl + "comments?api_key=" + this.apiKey;
+
         try{
-            const response = await axios.get(this.baseUrl + "/comments");
-            console.log(response);
+            const commentsResponse = await axios.get(COMMENTS_API_URL);
+            const commentsData = commentsResponse.data;
+
+            //Sort the comments from newest to oldest
+            commentsData.sort(function(a, b) {
+                return b.timestamp - a.timestamp;
+            })
+
+            return commentsData;
         } catch(error){
-            console.error(error);
+            console.error("error getting comments: " + error);
         }
     }
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,26 +1,66 @@
 import {api_key, BandSiteApi} from "./band-site-api.js";
 
-const tester = new BandSiteApi(api_key);
-console.log("Key is: "+ tester.apiKey)
+const BandSite = new BandSiteApi(api_key);
+// console.log("Key is: "+ BandSite.apiKey)
+// console.log(await BandSite.getComments());
 
 const comments = [
-    {name: "Connor Walton", timestamp: "02/17/2021", text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
-    {name: "Emilie Beach", timestamp: "01/09/2021", text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
-    {name: "Miles Acosta", timestamp: "12/20/2020", text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
+    // {name: "Connor Walton", timestamp: "02/17/2021", text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
+    // {name: "Emilie Beach", timestamp: "01/09/2021", text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
+    // {name: "Miles Acosta", timestamp: "12/20/2020", text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
+    // await BandSite.getComments()
 ];
 
-function currentTimestamp(){
-    const date = new Date();
+//Calculates current timestamp when new comment is submitted
+// function currentTimestamp(){
+//     const date = new Date();
 
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
+//     const day = date.getDate();
+//     console.log(day)
+//     const month = date.getMonth() + 1;
+//     const year = date.getFullYear();
+
+//     if (day < 10){
+//         day = "0" + day.toString();
+//         console.log(day);
+//     }
+
+//     const currentDate = `${month}/${day}/${year}`;
+//     return currentDate;
+// }
+
+function convertEpochToReadable(epochTimestamp){
+    const date = new Date(epochTimestamp);
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
     const year = date.getFullYear();
 
-    const currentDate = `${month}/${day}/${year}`;
+    //add a zero to the front of each single-digit number
+    const currentDate = `${addZero(month)}/${addZero(day)}/${year}`;
     return currentDate;
 }
 
-function createComment(comment){
+function addZero(num){
+    if (num < 10){
+        num = "0" + num.toString();
+        return num;
+    }
+    else{
+        return num;
+    }
+}
+
+function currentTimestampEpoch(){
+    var currentDate = new Date();
+
+    // Get the timestamp in milliseconds
+    var timestamp = currentDate.getTime();
+
+    return timestamp;
+}
+
+function createComment(commentObj){
     const commentEl = document.createElement('li');
     commentEl.classList.add('comment-entry');
 
@@ -38,18 +78,18 @@ function createComment(comment){
     
     const commentEntryNameEl = document.createElement('h3');
     commentEntryNameEl.classList.add('comment-entry__name');
-    commentEntryNameEl.innerText = comment.name;//----------------------------------------------
+    commentEntryNameEl.innerText = commentObj.name;//----------------------------------------------
     
     const commentEntryTimeEl = document.createElement('h3');
     commentEntryTimeEl.classList.add('comment-entry__timestamp');
-    commentEntryTimeEl.innerText = comment.timestamp;//----------------------------------------------
+    commentEntryTimeEl.innerText = convertEpochToReadable(commentObj.timestamp);//----------------------------------------------
 
     const commentEntryCommentEl = document.createElement('div');
     commentEntryCommentEl.classList.add('comment-entry__comment');
 
     const commentEntryTextEl = document.createElement('p');
     commentEntryTextEl.classList.add('comment-entry__text');
-    commentEntryTextEl.innerText = comment.text;
+    commentEntryTextEl.innerText = commentObj.comment;//---------------------------------------------
 
     commentEl.appendChild(headshotContainerEl);
     commentEl.appendChild(commentEntryContainerEl);
@@ -68,35 +108,55 @@ function createComment(comment){
     return commentEl;
 }
 
-function renderComments() {
+//asynch function that renders comments on the page
+async function renderComments() {
     const commentsContainerEl = document.querySelector(".comment-entry__container")
-    commentsContainerEl.innerHTML ="";
-    comments.forEach(commentObj => {
+    commentsContainerEl.innerHTML = "";
+
+    try {
+        //wait for the Api to return the array of comments
+        const apiComments = await BandSite.getComments();
+        console.log(apiComments);
+
+        const userComments = apiComments.map(() => 
+            console.log()
+            
+        )
+
+        //iterate through comments array retrieved from API
+        apiComments.forEach(commentObj => {
         const newComment = createComment(commentObj);
         commentsContainerEl.appendChild(newComment);
     });
+    } catch (error) {
+        
+    }
+    
 }
 
-function handleFormSubmit(event){
-
+async function handleFormSubmission(event){
     event.preventDefault();
 
     //create an object using the values from event (submitting the form)
     const commentData = {
         name: event.target.fullName.value,
-        timestamp: currentTimestamp(),
-        text: event.target.commentText.value,
+        comment: event.target.commentText.value
+        // timestamp: currentTimestampEpoch().toString()
     };
 
-    //add new comment to beginning of existing comments array
-    comments.unshift(commentData);
-    
-    renderComments();
-
-    formEl.reset();
+    const jsonComment = JSON.stringify(commentData);
+    try {
+        await BandSite.postComment(jsonComment);
+        renderComments();
+        formEl.reset();
+    } catch (error) {
+        console.log("Error posting comment: ")
+    }
 
 }
 
 // const formEl = document.getElementById("new-comment__form");
 const formEl = document.querySelector(".new-comment__form");
-formEl.addEventListener("submit", handleFormSubmit);
+formEl.addEventListener("submit", handleFormSubmission);
+
+renderComments();
